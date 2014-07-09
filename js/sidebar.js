ee36203a82e5d22e27e2b74a69fbfd1409e3f0bb
johndmulhausen <johndylan@google.com>
2014-07-09 05:44:23 +0000

    Updating to GoogleDevDocs/googledevdocs.github.io@830b22e5dcc32906b9627de05194c54e5703ce04.

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,37 +1,3 @@
-function enableTabs() {
-  if ($(".kd-tabbed-horz").length > 0)
-  {
-    // there are tabs! go through headers; if nothing has class=selected, there's no default, so set one.
-    // also hook up click event to show/hide tab functionality
-    $(".kd-tabbed-horz").each(function(index){
-        var hasSelected = false;
-        var $this = $(this);
-        $this.children("header").each(function(headerIndex)
-        {
-          console.log("header #" + headerIndex);
-          if (this.hasClass("selected")) hasSelected=true;
-          this.click(switchTabs(index,headerIndex));
-        });
-        if (!hasSelected) $(".kd-tabbed-horz header").first().addClass("selected");
-    });
-  }
-}
-
-var tabString="";
-function switchTabs(tabSetIndex,tabIndex)
-{
-  console.log("switchTabs called;")
-  var $tabSet = $(".kd-tabbed-horz").index(tabSetIndex);
-  tabString = $tabSet.children("header").index(tabIndex).innerHTML;
-  console.log(tabString + " was clicked!")
-  $tabSet.children("header").each(function(thisTab){
-    if(thisTab==tabIndex) {
-      $(this).addClass("selected");
-    } else {
-      $(this).removeClass("selected");
-    }
-  })
-}
 
 $( document ).ready(function() {
 $(".tlw-control").click(function() {
@@ -55,9 +21,7 @@ $(".tlw-control").click(function() {
 $("pre").addClass("notranslate");
 $("code").addClass("notranslate");
 
-// Enable tabs
-enableTabs();
-
+// kicks off a search
 $("#searchButton").click(function() {
   window.location.replace("/search?q=" + $("#q").val());
 });
