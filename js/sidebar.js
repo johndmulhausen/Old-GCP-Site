c85836cbd0209fac0dbb431d1e38608b44cbd4cd
johndmulhausen <johndylan@google.com>
2014-07-08 22:33:26 -0700

    Update sidebar.js

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,3 +1,38 @@
+function enableTabs() {
+  if ($(".kd-tabbed-horz").length > 0)
+  {
+    // there are tabs! go through headers; if nothing has class=selected, there's no default, so set one.
+    // also hook up click event to show/hide tab functionality
+    $(".kd-tabbed-horz").each(function(index){
+        var hasSelected = false;
+        var $this = $(this);
+        $this.children("header").each(function(headerIndex){
+          if (this.hasClass("selected") hasSelected=true;
+          this.click(function(){
+            switchTabs(index,headerIndex);
+          })
+        });
+        if (!hasSelected) $(".kd-tabbed-horz header").first().addClass("selected");
+      }
+    })
+  }
+}
+
+var tabString="";
+function switchTabs(tabSetIndex,tabIndex)
+{
+  var $tabSet = $(".kd-tabbed-horz").index(tabSetIndex);
+  tabString = $tabSet.children("header").index(tabIndex).innerHTML;
+  console.log(tabString + " was clicked!")
+  $tabSet.children("header").each(function(thisTab){
+    if(thisTab==tabIndex) {
+      $(this).addClass("selected");
+    } else {
+      $(this).removeClass("selected");
+    }
+  })
+}
+
 $( document ).ready(function() {
 $(".tlw-control").click(function() {
   // user clicked on an arrow
@@ -19,6 +54,9 @@ $(".tlw-control").click(function() {
 // Prevent code from being borked by the Google Translate tool
 $("pre").addClass("notranslate");
 $("code").addClass("notranslate");
+
+// Enable tabs
+enableTabs();
 
 $("#searchButton").click(function() {
   window.location.replace("/search?q=" + $("#q").val());
